d5670a2d6b53e0805374ec523ca22ab3fa553d37
derekburgess <contact@derekburgess.com>
2023-10-18 21:25:58 -0400

    Improvements to collection and mimic

--- ec2_chum.py
+++ ec2_chum.py
@@ -5,53 +5,49 @@ import random
 import time
 
 # Configuration variables
-HOST = 'IP ADDR HERE'  # Replace with your EC2 instance's public IP
-PORT = 443  # Port to connect to
-CHUNK_SIZE = 4096  # Typical size for a chunk of file data
-MOCK_FILE_SIZE = 5242880  # Mock file size in bytes
-INTERVAL = 30  # Time in seconds to wait before running the script again
+HOST = 'IP ADDR HERE'
+PORT = 443
+CHUNK_SIZE = 4096
 
 def generate_random_data(size):
     return bytes([random.randint(0, 255) for _ in range(size)])
 
 def main():
-    # Calculate how many chunks need to be sent
+    mock_file_size_MB = float(input("Enter the file size in MB: "))
+    MOCK_FILE_SIZE = int(mock_file_size_MB * 1024 * 1024)
+
     total_chunks = MOCK_FILE_SIZE // CHUNK_SIZE
     if MOCK_FILE_SIZE % CHUNK_SIZE:
-        total_chunks += 1  # Account for the last smaller chunk
+        total_chunks += 1
 
-    # Create a socket object
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
         print(f"Connecting to {HOST}:{PORT}...")
         sock.connect((HOST, PORT))
 
-        # Create an unverified SSL context
         context = ssl._create_unverified_context()
         
-        # Upgrade socket to SSL/TLS
         with context.wrap_socket(sock, server_hostname=HOST) as ssock:
             print(f"Connected to {HOST}:{PORT} over TLS")
             
             with Progress() as progress:
-                file_transfer_task = progress.add_task("[cyan]Transferring...", total=total_chunks)
+                task = progress.add_task("[cyan]Transferring...", total=total_chunks)
                 
                 for i in range(total_chunks):
                     chunk_size = CHUNK_SIZE if (i < total_chunks - 1) else (MOCK_FILE_SIZE % CHUNK_SIZE)
                     data = generate_random_data(chunk_size)
                     ssock.sendall(data)
-                    progress.update(file_transfer_task, completed=i+1)
+                    progress.update(task, completed=i+1)
+
+                    # Introduce a random delay to simulate variable transmission rate
+                    #random_delay = random.uniform(0.05, 0.5)  # Random delay between 0.05 and 0.5 seconds
+                    random_delay = random.uniform(0.01, 0.1)  # Random delay between 0.01 and 0.1 seconds
+                    time.sleep(random_delay)
             
             print("File transfer simulation complete.")
-        
-        print(f"Waiting {INTERVAL} seconds to run again...")
-        
-        with Progress() as progress:
-            countdown_task = progress.add_task("[magenta]Counting down...", total=INTERVAL)
-            
-            for i in range(INTERVAL):
-                time.sleep(1)
-                progress.update(countdown_task, completed=i+1)
 
 if __name__ == "__main__":
     while True:
         main()
+        repeat = input("Would you like to run again? (y/n): ")
+        if repeat.lower() != 'y':
+            break

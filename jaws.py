b6a3cb7ab6c9c485c20512dc05e80c266ca68869
derekburgess <contact@derekburgess.com>
2024-01-07 12:16:59 -0500

    Update

--- jaws.py
+++ jaws.py
@@ -5,7 +5,7 @@ from sklearn.metrics import classification_report, accuracy_score
 
 file_path = './data/sets/packets_.csv'
 data = pd.read_csv(file_path, low_memory=False)
-data = data[data['dst_ip'] != '192.168.99.10'] # Remove specific IP address
+data = data[data['dst_ip'] != 'IP ADDR'] # Remove specific IP address
 
 features = ['dst_port', 'size']
 target = 'dst_ip'
